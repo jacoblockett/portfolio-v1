98e5f01a77eaa04ac103a3f3a3a82475a18442b1
Jacob <jacobwyattlockett@gmail.com>
2020-01-08 13:45:57 -0500

    connecting to mail server

--- public/index.js
+++ public/index.js
@@ -124,3 +124,27 @@ function navigate(type, project) {
     }
   }
 }
+
+async function send() {
+  const name = document.getElementById('contact-name')
+  const email = document.getElementById('contact-email')
+  const message = document.getElementById('contact-message')
+  const emailReg = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/, 'i')
+
+  if (emailReg.test(email.value)) {
+    await fetch('https://jl-mailer.herokuapp.com/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      referrer: 'no-referrer',
+      body: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        sub: null,
+        msg: message.value
+      })
+    })
+    // const json = await response.json()
+  } else {
+    alert('Bad email!')
+  }
+}
